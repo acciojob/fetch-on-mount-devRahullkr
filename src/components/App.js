7d1383361cf8312f8be6d9cb98c3e1db6a27f438
Rahulllkr <rahulkumar8604745@gmail.com>
2026-02-27 10:38:23 +0530

    dataFetched

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,32 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      if (!res.ok) {
+        throw new Error("Data Not Fetched");
+      }
+      const dataFetched = await res.json();
+      console.log(dataFetched);
+      setData(dataFetched);
+    };
+    fetchData()
+  }, []);
   return (
     <div>
-        {/* Do not remove the main div */}
-        <p>Hello</p>
+      {/* Do not remove the main div */}
+      {data.map((item, index) => (
+        <div key={index}>
+          <h1>{item.title}</h1>
+          <p>{item.body}</p>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
